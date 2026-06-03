789117a05a85ad2c3e9c2c54236b6808773f9105
Rohan Punekar <rohan09punekar@gmail.com>
2026-06-03 14:47:34 +0530

    Connected javascript to registration page and added password verification

---
+++ register.js
@@ -0,0 +1,22 @@
+function register_company(event) {
+    event.preventDefault();
+
+    let company_name = document.getElementById("company_name").value.trim();
+    let business_type = document.getElementById("business_type").value;
+    let admin_name = document.getElementById("admin-name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value; 
+    let c_password= document.getElementById("confirm-password").value;
+
+    if (!company_name || !admin_name || !email) {
+        alert("Enter all required fields")
+    }
+    else if (password.length < 8) {
+        alert("Password must contain 8 or more characters");
+        return;
+    }
+    else if (password !== c_password) {
+        alert("Password does not match with Confirm Password");
+        return;
+    }   
+}
